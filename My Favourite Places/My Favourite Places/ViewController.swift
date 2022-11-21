78a9c13b189058a904181817061531d9b86bf9b9
mateusz464 <58775549+mateusz464@users.noreply.github.com>
2022-11-21 16:20:17 +0000

    Started new lab

---
+++ My Favourite Places/My Favourite Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Favourite Places
+//
+//  Created by Mateusz Golebiowski on 15/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
