3b820cb588c94020b3c59bcfb3b1c796a3f3f5aa
mateusz464 <58775549+mateusz464@users.noreply.github.com>
2022-11-29 16:04:56 +0000

    Added the JSON DataModel

--- New Brighton Murals/New Brighton Murals/DataModel.swift
+++ New Brighton Murals/New Brighton Murals/DataModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct image {
+    let id: Int
+    let filename: String
+}
+
+struct muralsData {
+    let id: Int
+    let title: String
+    let artist: String
+    let info: String
+    let thumbnail: URL
+    let lat: Double
+    let lon: Double
+    let enabled: Int
+    let lastModified: Date
+    let images: [image]
+}
