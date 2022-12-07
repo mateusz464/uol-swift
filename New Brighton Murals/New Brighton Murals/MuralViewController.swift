78e50aa7f1701e68926369220b7cb31d0370aacb
mateusz464 <58775549+mateusz464@users.noreply.github.com>
2022-12-07 14:54:27 +0000

    Populated the MuralViewController

--- New Brighton Murals/New Brighton Murals/MuralViewController.swift
+++ New Brighton Murals/New Brighton Murals/MuralViewController.swift
@@ -10,9 +10,43 @@ import MapKit
 
 class MuralViewController: UIViewController {
     
+    var currentTitle: String?
+    var artist: String?
+    var info: String?
+    var images: [String]?
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var artistLabel: UILabel!
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    @IBOutlet weak var imageBox: UIImageView!
+    
+    @IBOutlet weak var scOutlet: UISegmentedControl!
+    
+    @IBAction func scAction(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
-        // Add
+        
+        titleLabel.text = currentTitle ?? "No Title"
+        artistLabel.text = artist ?? "No Artist"
+        infoLabel.text = info ?? "No Info"
+        
+        if images != nil {
+            var image: UIImage?
+            let urlString = images![0]
+            
+            let url = NSURL(string: urlString)! as URL
+            if let imageData: NSData = NSData(contentsOf: url){
+                image = UIImage(data: imageData as Data)
+            }
+            
+            imageBox.image = image
+        }
+        
     }
     
 }
